43bc230f578638324462695648e059a58360cc4a
Keerthana <u.keerthana@gmail.com>
2022-06-06 16:33:03 +0200

    new change

--- ICL.Core/StructuralAnalysis/FEM.cs
+++ ICL.Core/StructuralAnalysis/FEM.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ICL.Core
+namespace ICL.Core.StructuralAnalysis
 {
-    public class Class1
+    internal class FEM
     {
     }
 }

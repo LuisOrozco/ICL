cee6f014fcd8ceeb3d6309dbf2d9ed4460314851
Keerthana <u.keerthana@gmail.com>
2022-06-06 15:47:42 +0200

    Add project files.

---
+++ ICL.GH/ICL.GHInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace ICL.GH
+{
+    public class ICL_GHInfo : GH_AssemblyInfo
+    {
+        public override string Name => "ICL.GH";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("D0676205-F469-47A6-8F52-2BEBB175BA2E");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}

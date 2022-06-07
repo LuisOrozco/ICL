c83bd74cf5454f80afa47370f58590f955cdc3e6
Keerthana <u.keerthana@gmail.com>
2022-06-07 16:02:19 +0200

    restructureing

--- ICL.GH/ICL.GHInfo.cs
+++ ICL.GH/ICL.GHInfo.cs
@@ -13,14 +13,14 @@ namespace ICL.GH
         public override Bitmap Icon => null;
 
         //Return a short string describing the purpose of this GHA library.
-        public override string Description => "";
+        public override string Description => "Grasshopper plug in of ICL method";
 
         public override Guid Id => new Guid("D0676205-F469-47A6-8F52-2BEBB175BA2E");
 
         //Return a string identifying you or your company.
-        public override string AuthorName => "";
+        public override string AuthorName => "Keerthana Udaykumar";
 
         //Return a string representing your preferred contact details.
-        public override string AuthorContact => "";
+        public override string AuthorContact => "u.keerthana@gmail.com";
     }
 }

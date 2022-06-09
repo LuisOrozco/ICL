0a308b970980919d0c639281ca928687c6721a6d
Keerthana <u.keerthana@gmail.com>
2022-06-09 18:07:13 +0200

    compute agent start pos completed

--- ICL.Core/AgentPos/BeamInitializeAgents.cs
+++ ICL.Core/AgentPos/BeamInitializeAgents.cs
@@ -18,7 +18,46 @@ namespace ICL.Core.AgentPos
 
     public class BeamInitializeAgents
     {
-        public string colour = "red";
+        public int ColumnNumbers;
+        public List<Point3d> EnvironmentBoundary;
+        public BeamInitializeAgents(int columnNumbers, List<Point3d> environmentBoundary)
+        {
+            this.ColumnNumbers = columnNumbers;
+            this.EnvironmentBoundary = environmentBoundary;
+        }
+
+        public List<Point3d> ComputeColumnStartPos()
+        {
+            List<Point3d> columnStartPos = new List<Point3d>();
+
+            if (this.ColumnNumbers == this.EnvironmentBoundary.Count)
+            {
+                foreach (Point3d point in this.EnvironmentBoundary)
+                {
+                    columnStartPos.Add(point);
+                }
+            }
+
+            if (this.ColumnNumbers < this.EnvironmentBoundary.Count && this.ColumnNumbers > 0)
+            {
+                Line line = new Line(this.EnvironmentBoundary[0], this.EnvironmentBoundary[1]);
+                Point3d midPoint = line.PointAt(0.5);
+                columnStartPos.Add(midPoint);
+            }
+
+            if (this.ColumnNumbers > this.EnvironmentBoundary.Count)
+            {
+                Line line = new Line(this.EnvironmentBoundary[0], this.EnvironmentBoundary[1]);
+                Point3d[] points;
+                var columnPos = line.ToNurbsCurve().DivideByCount((this.ColumnNumbers - 1), true, out points);
+                foreach (var pt in points)
+                {
+                    columnStartPos.Add(new Point3d(pt[0], pt[1], pt[2]));
+                }
+
+            }
+            return columnStartPos;
+        }
 
     }
 }

062bcfd79f27318a71bfe90e5a926e1eeb831726
Keerthana <u.keerthana@gmail.com>
2022-06-10 11:28:40 +0200

    docs for ICL.Core.AgentPos.BeamInitializeAgents

--- ICL.Core/AgentPos/BeamInitializeAgents.cs
+++ ICL.Core/AgentPos/BeamInitializeAgents.cs
@@ -14,11 +14,16 @@ namespace ICL.Core.AgentPos
     /// <summary>
     /// The AgentPos namespace contains fundamental types that define commonly used value types and classes for computing the column agent start pos
     /// </summary>
-    //internal class NamespaceDoc { }
 
     public class BeamInitializeAgents
     {
+        /// <summary>
+        /// number of columns at start 
+        /// </summary>
         public int ColumnNumbers;
+        /// <summary>
+        /// List of Beam boundary points (length will always be 2, not more, not less)
+        /// </summary>
         public List<Point3d> EnvironmentBoundary;
         public BeamInitializeAgents(int columnNumbers, List<Point3d> environmentBoundary)
         {
@@ -26,11 +31,19 @@ namespace ICL.Core.AgentPos
             this.EnvironmentBoundary = environmentBoundary;
         }
 
+        /// <summary>
+        /// Computes the start pos of columns in the Beam base on the number of start columns (cann never be 0)
+        /// </summary>
         public List<Point3d> ComputeColumnStartPos()
         {
             List<Point3d> columnStartPos = new List<Point3d>();
 
-            if (this.ColumnNumbers == this.EnvironmentBoundary.Count)
+            if (this.ColumnNumbers == 0)
+            {
+                RhinoApp.WriteLine("number of column Numbers must be greater than 0");
+            }
+
+            else if (this.ColumnNumbers == this.EnvironmentBoundary.Count)
             {
                 foreach (Point3d point in this.EnvironmentBoundary)
                 {
@@ -38,14 +51,14 @@ namespace ICL.Core.AgentPos
                 }
             }
 
-            if (this.ColumnNumbers < this.EnvironmentBoundary.Count && this.ColumnNumbers > 0)
+            else if (this.ColumnNumbers < this.EnvironmentBoundary.Count && this.ColumnNumbers > 0)
             {
                 Line line = new Line(this.EnvironmentBoundary[0], this.EnvironmentBoundary[1]);
                 Point3d midPoint = line.PointAt(0.5);
                 columnStartPos.Add(midPoint);
             }
 
-            if (this.ColumnNumbers > this.EnvironmentBoundary.Count)
+            else if (this.ColumnNumbers > this.EnvironmentBoundary.Count)
             {
                 Line line = new Line(this.EnvironmentBoundary[0], this.EnvironmentBoundary[1]);
                 Point3d[] points;
@@ -54,8 +67,8 @@ namespace ICL.Core.AgentPos
                 {
                     columnStartPos.Add(new Point3d(pt[0], pt[1], pt[2]));
                 }
-
             }
+
             return columnStartPos;
         }
 

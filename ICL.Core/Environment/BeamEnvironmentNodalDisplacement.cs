16cd4584a3b51d76226e9f008a3ba1a4fdd2fb59
Keerthana <u.keerthana@gmail.com>
2022-06-10 14:01:16 +0200

    BeamEnvironmentNodalDisplacement Dev

---
+++ ICL.Core/Environment/BeamEnvironmentNodalDisplacement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry;
+
+namespace ICL.Core.Environment
+{
+    public class BeamEnvironmentNodalDisplacement
+    {
+        ///dict of  nodal displacements 
+        Dictionary<double, Point3d> NodalDisplacement = new Dictionary<double, Point3d>();
+
+        ///list of positions of the agent 
+        List<Point3d> AgentPositions = new List<Point3d>();
+
+        ///list of start positions of the agents 
+        List<Point3d> AgentStartPositons = new List<Point3d>();
+
+        ///beam environment boundary points
+        List<Point3d> EnvironmentBoundary = new List<Point3d>();
+
+        ///new instance of EnvironmentNodalNodalDisplacement
+        public BeamEnvironmentNodalDisplacement(List<Point3d> agentPositions, List<Point3d> environmentBoundary)
+        {
+            this.AgentStartPositons = this.AgentPositions = agentPositions;
+            this.EnvironmentBoundary = environmentBoundary;
+        }
+
+
+    }
+}

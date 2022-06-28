8b5e880f88fca4e83fabde2cf192e1a052186912
KEERTHANAUDAY <u.keerthana@gmail.com>
2022-06-28 18:27:46 +0200

    MaxDisplacementBehaviour Dev set up

--- ICL.Core/AgentBehaviors/MaxDisplacementBehavior.cs
+++ ICL.Core/AgentBehaviors/MaxDisplacementBehavior.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ICD.AbmFramework.Core.Agent;
+using ICD.AbmFramework.Core.Behavior;
+using ICD.AbmFramework.Core.AgentSystem;
+using ICD.AbmFramework.Core.Environments;
+using Rhino.Geometry;
 
-namespace ICL.Core.AgentBasedModelling
+namespace ICL.Core.AgentBehaviors
 {
-    internal class MaxDisplacementBehavior
+    public class MaxDisplacementBehavior : BehaviorBase
     {
+        //public variables 
+        public Dictionary<int, List<Point3d>> NodalDisplacemenets = new Dictionary<int, List<Point3d>>();
+
+        /// <summary>
+        /// Constructs a new instance of the Boid cohesion behavior.
+        /// </summary>
+        public MaxDisplacementBehavior(Dictionary<int, List<Point3d>> nodalDisplacemenets)
+        {
+            this.NodalDisplacemenets = nodalDisplacemenets;
+        }
+        public override void Execute(AgentBase agent)
+        {
+            CartesianAgent columnAgent = (CartesianAgent)agent;
+            CartesianAgentSystem cartesianSystem = (CartesianAgentSystem)(columnAgent.AgentSystem);
+            CartesianEnvironment cartesianEnvironment = cartesianSystem.CartesianEnvironment;
+
+            // define cartesian agent 
+            // define cartesian agent system 
+            // define cartesian environment 
+
+            //identify agent's neighbour node with max displacement 
+            //get vector to move towards max displacement node
+
+            //move agent by magnitue of node divisions and direction of max displacement
+
+            throw new NotImplementedException();
+        }
     }
 }

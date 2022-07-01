5f7c3c6e4c2f2dc99541d0910b139a729357546d
KEERTHANAUDAY <u.keerthana@gmail.com>
2022-07-01 18:31:15 +0200

    beamsolver update

--- ICL.Core/Solver/BeamSolver.cs
+++ ICL.Core/Solver/BeamSolver.cs
@@ -4,28 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Rhino;
+using ICD.AbmFramework.Core.AgentSystem;
+using ICL.Core.Environment;
+using ICD.AbmFramework.Core.Agent;
+using ICD.AbmFramework.Core.Behavior;
 
 namespace ICL.Core.Solver
 {
     internal class NamespaceDoc { }
 
     public class BeamSolver
-    {   
-        ///the assembly part
-        ///This is an assembly class 
-        ///call agent: sets agent start position  
-        ///call environment call: sets start enevironment position
-        ///call agent system 
+    {
+        // public attributes 
+        public List<AgentSystemBase> AgentSystems;
 
-        ///method solver
-        ///make single-step-solver
-        ///preexecute
-        ///execute
-        ///postexecute
-        ///addandremoveagents 
-        ///call FEM
-        ///callFEA
-        ///update environment
-        
+        public int IterationCount = 0;
+
+        public BeamEnvironmentNodalDisplacement BeamStructuralData;
+
+        // Method for starting solver from a new list
+        public BeamSolver()
+        {
+            this.AgentSystems = new List<AgentSystemBase>();
+        }
+
+        ///Method0:
+        ///<summary>
+        ///Method for starting solver from an exisiting list of agents
+        ///</summary>
+        public BeamSolver(List<AgentSystemBase> agentSystems)
+        {
+            this.AgentSystems = agentSystems;
+
+            foreach (AgentSystemBase agentSystem in this.AgentSystems)
+            {
+                foreach (AgentBase agent in agentSystem.Agents)
+                {
+                    foreach (BehaviorBase behavior in agent.Behaviors)
+                    {
+                        behavior.Solver = BeamSolver; //solve from here //may be put the file outside the folder?
+                    }
+                }
+            }
+
+
+
+        }
+        ///List<Point3d> agentPositionsUpdate =  List<Point3d>()
+        ///foreach (CartesianAgent agent in this.AgentSystems)
+        ///agentPositionsUpdate.Add(agent.Positions)
+        /// this.beamEnvNodalDisps.AgentPositions = agentPositionsUpdate
+        /// this.beamEnvNodalDisps.PreExecute
+        /// this.beamEnvNodalDisps.Execute
+        /// foreach agentsystem in agentSystem
+        /// foreach agent in agent system
+        /// foreach behavor in agent.behavior
+        /// behavior.nodalsiap = this.beamEnvNodalDisps.Nodaldisp
+
     }
 }

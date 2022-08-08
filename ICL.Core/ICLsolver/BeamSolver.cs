135f0dd9804243cd85fb7a2a864a9b9a4701901d
KEERTHANAUDAY <u.keerthana@gmail.com>
2022-08-08 16:10:51 +0200

    solver inheritance update

---
+++ ICL.Core/ICLsolver/BeamSolver.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino;
+using Rhino.Geometry;
+
+using ICD.AbmFramework.Core.AgentSystem;
+using ICD.AbmFramework.Core;
+using ICD.AbmFramework.Core.Agent;
+//using ICD.AbmFramework.Core.Solver;
+
+using ICL.Core.AgentSystem;
+
+
+namespace ICL.Core.ICLsolver
+{
+    public class BeamSolver : Solver
+    {
+
+        /// <summary>  solver with display conduit</summary>
+        public void ICLbeamSolverExecute()
+        {
+            //IterationCount++;
+
+            foreach (AgentSystemBase agentSystem in this.AgentSystems)
+            {
+                agentSystem.PreExecute();
+            }
+            //    the parent class classes ABM methods not ICL.rewrite this part as well
+
+
+            //    foreach (ICLagentSystemBase agentSystem in AgentSystems)
+            //        if (!agentSystem.IsFinished()) agentSystem.Execute();
+
+            //    foreach (ICLagentSystemBase agentSystem in AgentSystems)
+            //        if (!agentSystem.IsFinished()) agentSystem.PostExecute();
+
+            //    foreach (ICLcartesianAgentSystem agentSystem in AgentSystems)
+            //    {
+            //        if (!agentSystem.IsFinished())
+            //        {
+            //            List<Point3d> updatedAgentPositions = UpdatedPositions(agentSystem);
+            //            agentSystem.CartesianEnvironment.AgentStartPositons = updatedAgentPositions;
+            //            agentSystem.CartesianEnvironment.UpdateEnvironment();
+            //        }
+        }
+        public List<Point3d> UpdatedPositions(ICLcartesianAgentSystem agentSystem)
+        {
+            List<Point3d> agentPositionsUpdate = new List<Point3d>();
+
+            foreach (CartesianAgent agent in agentSystem.Agents)
+            {
+                Point3d point = agent.Position;
+                agentPositionsUpdate.Add(point);
+            }
+            return agentPositionsUpdate;
+        }
+
+    }
+}
+

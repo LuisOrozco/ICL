9aef5951b8145b690f1b05e2e1e375c90a9853a2
KEERTHANAUDAY <u.keerthana@gmail.com>
2022-09-21 21:34:17 +0200

    slab max displacement update

--- ICL.Core/AgentBehaviors/SlabMaxDisplacementBehavior.cs
+++ ICL.Core/AgentBehaviors/SlabMaxDisplacementBehavior.cs
@@ -42,28 +42,54 @@ namespace ICL.Core.AgentBehaviors
 
             if (this.NodalDisplacemenets.Count == 0)
             {
-                //neighborNodes = FindNeightbors(columnAgent.Position, this.StartNodalDisplacemenets, out int agentPosNodeIndex);
+                List<int> agentPosNodeIndex = new List<int>();
+                neighborNodes = FindNeighborsSlab(columnAgent.Position, this.SlabGeo, StartNodalDisplacemenets, ref agentPosNodeIndex);
             }
 
             if (this.NodalDisplacemenets.Count > 0)
             {
-                //neighborNodes = FindNeightbors(columnAgent.Position, this.StartNodalDisplacemenets, out int agentPosNodeIndex);
+                List<int> agentPosNodeIndex = new List<int>();
+                neighborNodes = FindNeighborsSlab(columnAgent.Position, this.SlabGeo, this.NodalDisplacemenets, ref agentPosNodeIndex);
             }
 
 
         }
         //Method add moves 
         //search neighbours 
-        public void FindNeighborsSlab(Point3d agentPoistion)
+        public Dictionary<string, List<Point3d>> FindNeighborsSlab(Point3d agentPoistion, Mesh mesh, Dictionary<int, List<Point3d>> displacementsDict, ref List<int> agentPosInd)
         {
-            int agentPositionIndex;
+            List<int> agentPositionIndex = new List<int>();
             for (int i = 0; i < this.SlabGeo.TopologyVertices.Count; i++)
             {
                 if (agentPoistion.EpsilonEquals(SlabGeo.TopologyVertices[i], 0.001))
                 {
-                    agentPositionIndex = i;
+                    agentPositionIndex.Add(i);
+
                 }
             }
+            agentPosInd.Add(agentPositionIndex[0]);
+            int[] neighborInd = mesh.TopologyVertices.ConnectedTopologyVertices(agentPositionIndex[0]);
+            List<Point3d> connectedVertices = new List<Point3d>();
+            foreach (int i in neighborInd)
+            {
+                Point3d pt = mesh.Vertices.Point3dAt(i);
+                connectedVertices.Add(pt);
+            }
+
+            Dictionary<string, List<Point3d>> neighborNodes = new Dictionary<string, List<Point3d>>();
+            for (int i = 0; i < connectedVertices.Count; i++)
+            {
+                foreach (var item in displacementsDict.Values)
+                {
+                    if (connectedVertices[i] == item[0])
+                    {
+                        //neighborNodalDisp.Add(item[1]);
+                        neighborNodes.Add(i.ToString(), new List<Point3d>() { item[0], item[1] });
+                    }
+                }
+            }
+
+            return neighborNodes;
             //find vertext index from agent position 
             //find connected points
             //for each connected point find its respective nodal displaceemnt value 
